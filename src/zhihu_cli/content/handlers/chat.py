c1ded46b6f360649f3e21c6bc0e2ee3df0986b11
KrisTHL181 <KrisTHL181@outlook.com>
2026-04-20 00:17:51 +0800

    feat: implement chat inbox and message handling functionality

---
+++ src/zhihu_cli/content/handlers/chat.py
@@ -0,0 +1,87 @@
+from .requests import session
+from . import fmt_time
+from .requests import get_page_entities
+from typing import List, Dict, Optional
+
+def get_inbox() -> List[Dict]:
+    messages = []
+    resp = session.get("https://www.zhihu.com/api/v4/inbox", timeout=15)
+    resp.raise_for_status()
+
+    inbox = resp.json()["data"]
+    for message in inbox:
+        messages.append(
+            {
+                "id": message.get("participant", {}).get("id"),
+                "url_token": message.get("url_token", ""),
+                "from": message.get("participant", {}).get("name", "未知用户"),
+                "snippet": message.get("snippet", "(无内容)"),
+                "updated_time": fmt_time(message.get("updated_time")),
+                "message_count": message.get("message_count", 0),
+                "unread_count": message.get("unread_count", 0),
+            }
+        )
+    return messages
+
+def _parse_messages_page(data: Dict) -> tuple[List[Dict], Optional[str], tuple]:
+    data_obj = data.get('data', {})
+    messages = data_obj.get("messages", [])
+    if not messages:
+        return [], None, (None, None)
+
+    receiver_name = data_obj.get("receiver", {}).get("name", "Unknown")
+    sender_name = data_obj.get("sender", {}).get("name", "Unknown")
+
+    page_msgs = []
+    for msg in messages:
+        if msg.get('type') != "message":
+            continue
+
+        role = "them" if msg.get('user_type') == 'sender' else "me"
+        content = msg.get('text', '')
+        time_str = fmt_time(msg.get('created_time'))
+        page_msgs.append({
+            "role": role,
+            "content": content,
+            "time": time_str
+        })
+
+    last_id = messages[-1].get('id')
+    return page_msgs, last_id, (receiver_name, sender_name)
+
+def iter_chat_history(chat_id: str):
+    current_url = f"https://www.zhihu.com/api/v4/chat?sender_id={chat_id}"
+    
+    while current_url:
+        resp = session.get(current_url, timeout=15)
+        resp.raise_for_status()
+        
+        breakpoint()
+        data = resp.json()
+        
+        page_msgs, _, _ = _parse_messages_page(data)
+        if not page_msgs:
+            break
+            
+        yield from page_msgs
+        
+        paging = data.get('paging', {})
+
+        if paging.get('is_end', True) or len(page_msgs) < 20:
+            current_url = None
+        else:
+            current_url = paging.get('next')
+
+def send_text_message(their_id: str, content: str) -> dict:
+    resp = session.post("https://www.zhihu.com/api/v4/chat", json={
+        "content_type": 0,
+        "text": content,
+        "receiver_id": their_id
+    }, timeout=15)
+    
+    data = resp.json()
+    if resp.status_code == 403 and "error" in data.keys():
+        raise RuntimeError(f"Failed to send message: {data['error']['message']}")
+    resp.raise_for_status()
+
+    return data
---
+++ src/zhihu_cli/content/handlers/imchat.py
@@ -0,0 +1,58 @@
+import json
+import time
+from paho.mqtt import client as mqtt_client
+from paho.mqtt.enums import CallbackAPIVersion
+import queue
+from .cache_manager import cache_manager
+from .requests import get_page_entities
+
+NOTIFICATION_TOPIC = "zhihu/notification/badge/web/v1/{USER_HASH}/"
+IMCHAT_TOPIC = "zhihu/message/v1/im/user/{USER_HASH}/"
+
+def get_pm_mqtt_topic(url_token: str) -> str:
+    entities = get_page_entities(f"https://www.zhihu.com/people/{url_token}")
+    item = entities["users"]
+    return next(iter(item))
+
+class ZhihuMessageListener:
+    def __init__(self, url_token: str, topic: str, incognito: bool = False):
+        self.url_token = url_token
+        self.user_hash = get_pm_mqtt_topic(url_token)
+        self.msg_queue = queue.Queue()
+
+        self.topic = topic.replace("{USER_HASH}", self.user_hash)
+
+        self.broker = "mqtt-web.zhihu.com"
+        self.port = 443
+        self.client_id = f"mqttjs_{int(time.time() * 1000):x}"[:15]
+        self.incognito = incognito
+        self.client = self._connect()
+
+    def _connect(self):
+        client = mqtt_client.Client(CallbackAPIVersion.VERSION2, self.client_id, transport='websockets')
+        client.tls_set()
+
+        headers = cache_manager.load_headers()
+
+        ws_path = "/mqtt?client_info=OS%3DWeb&user_group=zhihu_web"
+        client.ws_set_options(path=ws_path, headers=headers)
+
+        client.on_connect = self.on_connect
+        client.on_message = self.on_message
+
+        client.connect(self.broker, self.port)
+        return client
+
+    def on_connect(self, client, userdata, flags, reason_code, properties):
+        if reason_code.is_failure:
+            raise ConnectionError(f"Failed to connect to Zhihu MQTT Broker. Reason: {reason_code} (Code: {reason_code.value})")
+        else:
+            client.subscribe(self.topic)
+
+    def on_message(self, client, userdata, msg):
+        raw_payload = msg.payload.decode('utf-8')
+        data = json.loads(raw_payload)
+        self.msg_queue.put(data)
+
+    def start(self):
+        self.client.loop_forever()
